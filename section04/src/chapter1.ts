11e0c1a1034dd494ea51eb21d5ca0f23d4181f90
KangJiUng <woong236406@gmail.com>
2025-11-29 16:08:59 +0900

    chapter01

---
+++ section04/src/chapter1.ts
@@ -0,0 +1,23 @@
+// 함수 타입 표현식
+
+type Operation = (a: number, b: number) => number; // 방법 1
+
+const add: (a: number, b: number) => number = (a, b) => a + b; // 방법 2
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: Operation = (a, b) => a / b;
+
+// 호출 시그니처(콜 시그니처)
+
+type Operation2 = {
+  (a: number, b: number): number;
+  name: string;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
+
+add2();
+add2.name;
